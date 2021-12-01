de4fd7b501f44c9b9f9a90621fd7b76c013d6f1b
dkomaristiy <DKomaristiy@yandex.ru>
2021-12-01 15:30:03 +0300

    deleted unnecessary files

--- Server/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/dmitriy/DataArt_It_Lead/Projects/Server
-# Build directory: /home/dmitriy/DataArt_It_Lead/Projects/Server/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
